f9d591497340211c0e62f26dc616c7361e99bf9f
BrennerFerreira <brennercsferreira@gmail.com>
2021-08-27 13:53:58 -0300

    feat: add loading rive animation

---
+++ lib/app/theme/app_assets.dart
@@ -0,0 +1,6 @@
+/// [AppAssets] holds the reference to all assets used in the app.
+class AppAssets {
+  /// [loadingAnimation] redirects to the location of the loading_icon.riv
+  /// file.
+  static const loadingAnimation = 'assets/loading_icon.riv';
+}
--- lib/shared/widgets/loading_widget/loading_widget.dart
+++ lib/shared/widgets/loading_widget/loading_widget.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+import '../../../app/theme/app_assets.dart';
 
 /// [LoadingWidget] this is the widget to show when there's a loading state in
 /// the app.
 class LoadingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: CircularProgressIndicator.adaptive(),
+    return Center(
+      child: SizedBox(
+        height: MediaQuery.of(context).size.width * 0.8,
+        width: MediaQuery.of(context).size.width * 0.8,
+        child: const RiveAnimation.asset(AppAssets.loadingAnimation),
+      ),
     );
   }
 }
